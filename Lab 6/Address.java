1d1899e6b262f9495b3ba208906f1a15da443356
strtdr <47079140+strtdr@users.noreply.github.com>
2020-01-09 11:44:28 +0200

    Create Address.java

---
+++ Lab 6/Address.java
@@ -0,0 +1,17 @@
+package Laborator6;
+
+public class Address {
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String apartment;
+
+    public Address(String country, String city, String street, String house, String apartment) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+    }
+}
