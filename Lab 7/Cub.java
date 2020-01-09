f73fe54fbe4fb4374b72575ec93a9749ea1e95cc
strtdr <47079140+strtdr@users.noreply.github.com>
2020-01-09 11:45:40 +0200

    Create Cub.java

---
+++ Lab 7/Cub.java
@@ -0,0 +1,14 @@
+package Laborator7;
+
+public class Cub implements IGeometricBody {
+
+    @Override
+   public float getVolume() {
+        return 8f;
+    }
+
+    @Override
+   public float getSurface() {
+        return 24f;
+    }
+}
